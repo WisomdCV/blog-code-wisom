a42551743eb6c86e61a4112f455e6d4bcf316a21
WisomdCV <wilsondcv711@gmail.com>
2024-11-20 06:15:39 -0500

    Commit 8
    Actualización de posts

--- src/main/java/com/wisom711/controller/PostController.java
+++ src/main/java/com/wisom711/controller/PostController.java
@@ -1,13 +1,18 @@
 package com.wisom711.controller;
 
-
+import com.wisom711.entity.PostEntity;
+import com.wisom711.entity.UserEntity;
 import com.wisom711.service.PostService;
+import com.wisom711.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 @Controller
 @RequestMapping("/post")
 public class PostController {
@@ -15,9 +20,23 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserService userService;
     @GetMapping("/home")
-    public String homePage(Model model){
-        model.addAttribute("posts", postService.getAllPost());
+    public String homePage(Model model) {
+        model.addAttribute("posts",postService.getAllPost());
         return "/posts/home";
+    }
+    @GetMapping("/new")
+    public String newPostPage() {
+        return "/posts/create-post";
+    }
+    @PostMapping("/create")
+    public String createPost(PostEntity post, HttpSession session) {
+        post.setCreatedAt(LocalDateTime.now());
+        UserEntity user = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString())).get();
+        post.setUser(user);
+        postService.createPost(post);
+        return "redirect:/post/home";
     }
 }
