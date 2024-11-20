a5558a490fc3c76bd1da2e37ec02fca4141308b1
WisomdCV <wilsondcv711@gmail.com>
2024-11-20 04:31:24 -0500

    Commit 6
    Endpoints de logeo y autenticación.
    (acceso a la vista home por usuario y deslogeo)

--- src/main/java/com/wisom711/controller/UserController.java
+++ src/main/java/com/wisom711/controller/UserController.java
@@ -3,12 +3,16 @@ package com.wisom711.controller;
 
 import com.wisom711.entity.UserEntity;
 import com.wisom711.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -37,4 +41,22 @@ public class UserController {
         return "/users/login";
     }
 
+    @GetMapping("/access")
+    public String access(HttpSession session) {
+        Optional<UserEntity> optionalUser = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString()));
+        if (optionalUser.isPresent()) {
+            session.setAttribute("user_session_id", optionalUser.get().getId());
+            return "redirect:/post/home";
+        } else {
+            return "redirect:/login";
+        }
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/login";
+    }
 }
