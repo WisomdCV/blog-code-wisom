04405da0edc3305016ce45d6cf78b31eae054c0a
WisomdCV <wilsondcv711@gmail.com>
2024-11-20 03:45:18 -0500

    Commit 4
    Controlador para user
    Diseñi de vistas de autenticación con Thymeleaf e implementación de las funcionalidades de registro e inicio de sesión.
    
    Mapeos de endpoints y registro de usuarios en la base de datos.

---
+++ src/main/java/com/wisom711/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.wisom711.controller;
+
+
+import com.wisom711.entity.UserEntity;
+import com.wisom711.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/record")
+    public String recordPage(){
+        return "/users/register";
+    }
+
+    @PostMapping("/register")
+    public String register(UserEntity user) {
+        userService.createUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = {"/login", "/"})
+    public String loginPage(){
+        return "/users/login";
+    }
+
+}
