04a765f6501b7010aec7a3858e3cbfc378f2f14f
WisomdCV <wilsondcv711@gmail.com>
2024-11-20 08:42:22 -0500

    Commit 12
    Editar eliminar y actualizar comentarios.

--- src/main/java/com/wisom711/controller/CommentController.java
+++ src/main/java/com/wisom711/controller/CommentController.java
@@ -10,9 +10,8 @@ import com.wisom711.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -42,5 +41,24 @@ public class CommentController {
 
         commentService.createComment(comment);
         return "redirect:/post/postPage/" + postId;
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editComment(@PathVariable Long id, Model model) {
+        CommentEntity comment = commentService.getCommentById(id).orElseThrow(() -> new IllegalArgumentException("¡Invalid comment id!"));
+        model.addAttribute("comment", comment);
+        return "/posts/update-comment";
+    }
+    @PostMapping("/update")
+    public String updateComment(@RequestParam("idComment") Long id, CommentEntity comment) {
+        CommentEntity commentDB = commentService.getCommentById(id).orElseThrow(() -> new IllegalArgumentException("¡Invalid comment id!"));
+        commentService.updateComment(id, comment);
+        return "redirect:/post/postPage/" + commentDB.getPost().getId();
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteComment(@PathVariable Long id) {
+        CommentEntity comment = commentService.getCommentById(id).orElseThrow(() -> new IllegalArgumentException("¡Invalid comment id!"));
+        commentService.deleteComment(id);
+        return "redirect:/post/postPage/" + comment.getPost().getId();
     }
 }
